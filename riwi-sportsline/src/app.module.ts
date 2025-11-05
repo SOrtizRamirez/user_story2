4f9086663676c6b84f95bee882634259eaa45f1c
josecortes99 <j.c.e555@hotmail.com>
2025-11-05 13:37:32 -0500

    feat: Creacion archivo .env con variables de entorno y configuracion de ConfigModule

--- riwi-sportsline/src/app.module.ts
+++ riwi-sportsline/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, //Hace que las variables esten disponibles en toda la app
+      envFilePath: '.env', //Ubicacion del archivo de variables .env
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
