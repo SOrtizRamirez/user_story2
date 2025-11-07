e054ce5b715dc593bbf93cf5105e59985ce3560d
josecortes99 <j.c.e555@hotmail.com>
2025-11-07 13:49:44 -0500

    continuar

--- riwi-sportsline/src/user/user.service.ts
+++ riwi-sportsline/src/user/user.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./entities/user.entity";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @Inye
+    )
+}
