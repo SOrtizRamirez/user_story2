a35dcbe7d60c41a02565cdbe5e1674870fc1edde
JoseMonsalve07 <00jsmonsalve@gmail.com>
2025-11-14 08:20:00 -0500

    feat: implement basic guards on roles and permissions finished (implement the guards in the controllers)

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,32 +1,44 @@
-import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('clients')
 export class ClientController {
     constructor(private readonly clientService: ClientService) {}
 
+    @UseGuards(RolesGuard)
+    @Roles('admin', 'seller')
     @Post()
     create(@Body() createClientDto: CreateClientDto) {
         return this.clientService.create(createClientDto);
     }
-
+    
+    @UseGuards(RolesGuard)
+    @Roles('admin', 'seller')
     @Get()
     findAll() {
         return this.clientService.findAll();
     }
 
+    @UseGuards(RolesGuard)
+    @Roles('admin', 'seller')
     @Get(':id')
     findOne(@Param('id') id: number) {
         return this.clientService.findOne(id);
     }
 
+    @UseGuards(RolesGuard)
+    @Roles('admin', 'seller')
     @Put(':id')
     update(@Param('id') id: number, @Body() updateClientDto: UpdateClientDto) {
         return this.clientService.update(id, updateClientDto);
     }
 
+    @UseGuards(RolesGuard)
+    @Roles('admin')
     @Delete(':id')
     remove(@Param('id') id: number) {
         return this.clientService.remove(id);
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,33 +1,44 @@
-import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
-import { Product } from './product.entity';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('products')
 export class ProductController {
     constructor(private readonly productService: ProductService) {}
 
+    @UseGuards(RolesGuard)
+    @Roles('admin')
     @Post()
     create(@Body() createProductDto: CreateProductDto) {
         return this.productService.create(createProductDto);
     }
 
+    @UseGuards(RolesGuard)
+    @Roles('admin', 'seller')
     @Get()
     findAll() {
         return this.productService.findAll();
     }
 
+    @UseGuards(RolesGuard)
+    @Roles('admin', 'seller')
     @Get(':id')
     findOne(@Param('id') id: string) {
         return this.productService.findOne(+id);
     }
-
+    
+    @UseGuards(RolesGuard)
+    @Roles('admin')
     @Put(':id')
     update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
         return this.productService.update(+id, updateProductDto);
     }
 
+    @UseGuards(RolesGuard)
+    @Roles('admin')
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.productService.remove(+id);
