7dde0113239a1e20043bfba6a03cb9173c5988e5
JoseMonsalve07 <00jsmonsalve@gmail.com>
2025-12-03 11:24:18 -0500

    feat: Implement guards and decorators to endpoints

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -2,41 +2,46 @@ import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nes
 import { ClientService } from './client.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
-import { RolesGuard } from 'src/common/guards/roles.guard';
+import { RbacGuard } from 'src/common/guards/rbac.guard';
+import { Permissions } from 'src/common/decorators/permissions.decorator';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('clients')
-@UseGuards(JwtAuthGuard ,RolesGuard)
+@UseGuards(JwtAuthGuard, RbacGuard)
 export class ClientController {
     constructor(private readonly clientService: ClientService) {}
     
-    @Roles('admin', 'seller')
+    @Roles('Admin', 'Seller')
+    @Permissions('Create')
     @Post()
     create(@Body() createClientDto: CreateClientDto) {
         return this.clientService.create(createClientDto);
     }
-
     
-    @Roles('admin', 'seller')
+    @Roles('Admin', 'Seller')
+    @Permissions('Read')
     @Get()
     findAll() {
         return this.clientService.findAll();
     }
     
-    @Roles('admin', 'seller')
+    @Roles('Admin', 'Seller')
+    @Permissions('Read')
     @Get(':id')
     findOne(@Param('id') id: number) {
         return this.clientService.findOne(id);
     }
     
-    @Roles('admin', 'seller')
+    @Roles('Admin', 'Seller')
+    @Permissions('Update')
     @Put(':id')
     update(@Param('id') id: number, @Body() updateClientDto: UpdateClientDto) {
         return this.clientService.update(id, updateClientDto);
     }
     
-    @Roles('admin')
+    @Roles('Admin')
+    @Permissions('Delete')
     @Delete(':id')
     remove(@Param('id') id: number) {
         return this.clientService.remove(id);
--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -2,40 +2,46 @@ import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nes
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import { RolesGuard } from 'src/common/guards/roles.guard';
+import { RbacGuard } from 'src/common/guards/rbac.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Permissions } from 'src/common/decorators/permissions.decorator';
 
 @Controller('order')
-@UseGuards(JwtAuthGuard, RolesGuard)
+@UseGuards(JwtAuthGuard, RbacGuard)
 export class OrderController {
     constructor(private readonly orderService: OrderService) {}
 
-    @Roles('admin', 'seller')
+    @Roles('Admin', 'Seller')
+    @Permissions('Create')
     @Post()
     create(@Body() createOrderDto: CreateOrderDto) {
         return this.orderService.create(createOrderDto);
     }
 
-    @Roles('admin', 'seller')
+    @Roles('Admin', 'Seller')
+    @Permissions('Read')
     @Get()
     findAll() {
         return this.orderService.findAll();
     }
 
-    @Roles('admin', 'seller')
+    @Roles('Admin', 'Seller')
+    @Permissions('Read')
     @Get(':id')
     findOne(@Param('id') id: string) {
         return this.orderService.findOne(+id);
     }
 
-    @Roles('admin', 'seller')
+    @Roles('Admin', 'Seller')
+    @Permissions('Update')
     @Put(':id')
     update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
         return this.orderService.update(+id, updateOrderDto);
     }
 
-    @Roles('admin')
+    @Roles('Admin')
+    @Permissions('Delete')
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.orderService.remove(+id);
