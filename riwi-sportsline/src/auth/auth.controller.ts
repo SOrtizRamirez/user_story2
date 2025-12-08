9798a931f68bcf2caaec301f5f1e9edce6505b44
Jose Cortes <j.c.e555@hotmail.com>
2025-12-08 06:45:12 -0500

    modulo autenticacion x-xapi-key

--- riwi-sportsline/src/auth/auth.controller.ts
+++ riwi-sportsline/src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -31,41 +32,14 @@ export class AuthController {
   refresh(@Req() req) {
     return this.authService.refresh(req.user);
   }
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth() {}
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return req.user;
+  }
 }
-
-// import { Controller, Post, Body } from '@nestjs/common';
-// import { AuthService } from './auth.service';
-// import { UserService } from '../user/user.service';
-// import { RegisterDto } from './dto/register.dto';
-// import { LoginDto } from './dto/login.dto';
-// import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
-
-// @ApiTags('auth') //  Agrupa los endpoints bajo "auth" en Swagger
-// @Controller('auth')
-// export class AuthController {
-//   constructor(
-//     private readonly authService: AuthService,
-//     private readonly userService: UserService,
-//   ) {}
-
-//   @Post('register')
-//   @ApiOperation({ summary: 'Registrar un nuevo usuario' })
-//   @ApiResponse({ status: 201, description: 'Usuario registrado correctamente' })
-//   @ApiResponse({ status: 400, description: 'Datos inválidos o usuario existente' })
-//   @ApiBody({ type: RegisterDto }) //  Muestra la estructura esperada del body
-//   async register(@Body() registerDto: RegisterDto) {
-//     const { name, email, password } = registerDto;
-//     const user = await this.userService.create(name, email, password);
-//     return { message: 'Usuario registrado correctamente', user };
-//   }
-
-//   @Post('login')
-//   @ApiOperation({ summary: 'Iniciar sesión de usuario' })
-//   @ApiResponse({ status: 200, description: 'Inicio de sesión exitoso (devuelve token JWT)' })
-//   @ApiResponse({ status: 401, description: 'Credenciales inválidas' })
-//   @ApiBody({ type: LoginDto })
-//   async login(@Body() loginDto: LoginDto) {
-//     const { email, password } = loginDto;
-//     return this.authService.login(email, password);
-//   }
-// }
